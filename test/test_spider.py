3b03f80e489f1e2f5e727701cd90f39c2188bc93
rongfudi <rongfudi@yeah.net>
2016-06-01 10:05:16 +0800

    add some test

--- test/test_spider.py
+++ test/test_spider.py
@@ -8,18 +8,30 @@ __mtime__ = '16/5/18'
 from src.base import BaseSpider
 from src.config import SinaConfig
 import time
+import unittest
 
 
-def main():
-    start = time.time()
-    spider = BaseSpider(config=SinaConfig)
-    # url = 'http://news.163.com/16/0517/19/BN9QMN1G00014PRF.html'
-    # spider.parse_xpath_content(url)
-    spider.run()
-    end = time.time()
-    print "called_time:%d" % (end - start)
+class SpiderTest(unittest.TestCase):
+    def test_one(self):
+        self.assertTrue(True)
 
-
+    def test_two(self):
+        self.assertFalse(False)
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
+
+
+# def main():
+#     start = time.time()
+#     spider = BaseSpider(config=SinaConfig)
+#     # url = 'http://news.163.com/16/0517/19/BN9QMN1G00014PRF.html'
+#     # spider.parse_xpath_content(url)
+#     spider.run()
+#     end = time.time()
+#     print "called_time:%d" % (end - start)
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
